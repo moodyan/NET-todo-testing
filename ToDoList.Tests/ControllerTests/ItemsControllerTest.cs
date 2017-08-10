81943a8201c01c4c122b7b83cef205437466a7ff
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-08-09 21:11:06 -0700

    Add testing with xUnit.

---
+++ ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Controllers;
+using ToDoList.Models;
+using Xunit;
+
+namespace ToDoList.Tests
+{
+    [Collection("ToDoListTests")]
+    class ItemsControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            ItemsController controller = new ItemsController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Get_ModelList_Index_Test()
+        {
+            //Arrange
+            ItemsController controller = new ItemsController();
+            IActionResult actionResult = controller.Index();
+            ViewResult indexView = controller.Index() as ViewResult;
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsType<List<Item>>(result);
+        }
+    }
+}
---
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Controllers;
+using ToDoList.Models;
+using Xunit;
+
+namespace ToDoList.Tests
+{
+    [Collection("ToDoListTests")]
+    public class ItemTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var item = new Item();
+            item.Description = "Wash the dog";
+
+            //Act
+            var result = item.Description;
+
+            //Assert
+            Assert.Equal("Wash the dog", result);
+        }
+
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            ItemsController controller = new ItemsController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Get_ModelList_Index_Test()
+        {
+            //Arrange
+            ItemsController controller = new ItemsController();
+            IActionResult actionResult = controller.Index();
+            ViewResult indexView = controller.Index() as ViewResult;
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsType<List<Item>>(result);
+        }
+    }
+}
